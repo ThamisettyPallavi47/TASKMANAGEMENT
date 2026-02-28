b8ef362c33559bfb72351675313049d236e20972
ThamisettyPallavi47 <pallavithammisetty@gmail.com>
2026-02-28 21:55:35 +0530

    Updated db.js to use environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,23 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/studentTaskDB");
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error("MongoDB connection failed", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/studentTaskDB");
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed", error.message);
     process.exit(1);
